80e9cbeba4d33f3f2d312ef79bab8a4019e5a8ba
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-08 17:06:43 -0400

    Local template references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -15,9 +15,10 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
     this.serverCreated.emit({
-      serverName: this.newServerName,
+      serverName: nameInput.value,
        serverContent: this.newServerContent
     });
   }
