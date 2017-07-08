dbf092c27ad11fa7de96af2ce256619164a5213d
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-08 18:26:07 -0400

    ngOnChanges

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,16 +13,22 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+
 
   constructor() {
     console.log('constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
   ngOnInit() {
     console.log('ngOnInit called!');
-
   }
 
 }
